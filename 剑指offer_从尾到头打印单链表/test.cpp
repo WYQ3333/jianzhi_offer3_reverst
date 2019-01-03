645c20a6bb93b39433ee47deb88734f0c1bba4cf
浅忆梦微凉 <3538922590@qq.com>
2019-01-03 22:35:54 +0800

    创建链表编译通过

--- 剑指offer_从尾到头打印单链表/test.cpp
+++ 剑指offer_从尾到头打印单链表/test.cpp
@@ -14,11 +14,14 @@ struct ListNode {
 
 class Solution {
 public:
+	Solution()
+		:_head(nullptr){
+	}
 	vector<int> printListFromTailToHead(ListNode* head) {
 		vector<int> arrayList;
 		int i = 0;
-		ListNode *pcur = head;
-		ListNode *pre = head;
+		ListNode *pcur = _head;
+		ListNode *pre = _head;
 		while (pcur!= nullptr){
 			pcur = pcur->next;
 			pre->next = head->next;
@@ -28,10 +31,45 @@ public:
 		}
 		return arrayList;
 	}
+	ListNode* BuyNewNode(int val){
+		ListNode *NewNode = (ListNode*)new int(val);
+		if (nullptr != NewNode){
+			return nullptr;
+		}
+		NewNode->next = nullptr;
+		NewNode->val = val;
+		return NewNode;
+	}
+	void CreateLinklist(int val){
+		ListNode *head = nullptr;
+		ListNode *NewNode = BuyNewNode(val);
+		if (NewNode == nullptr){
+			return;
+		}
+		NewNode->next = head;
+		head->next = NewNode;
+	}
+	~Solution(){
+		ListNode* pcur = _head;
+		ListNode* pre = _head;
+		while (pcur != nullptr){
+			pcur = pcur->next;
+			delete pre;
+			pre = pcur;
+		}
+	}
+public:
+	ListNode* _head;
 };
 
 
 int main(){
+	Solution s;
+	s.CreateLinklist(1);
+	s.CreateLinklist(2);
+	s.CreateLinklist(3);
+	s.CreateLinklist(4);
+	s.CreateLinklist(5);
 	system("pause");
 	return 0;
 }
